5d36f2bb61fd10e5b53c1c373634ba7d5c4b0662
David Feuer <David.Feuer@gmail.com>
2025-06-30 23:21:57 -0400

    Test ever so slightly
    We still need proper unit tests.

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,8 @@
 module Main (main) where
+import Data.Traversable.Unzip
+import Data.Bifunctor (bimap)
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  print . bimap length length $ unzipLazy [undefined :: (Int, Int), undefined, undefined]
+  print . bimap (take 3) (take 3) $ unzipLazy ((1 :: Int, 'a') : (2, 'b') : (3, 'c') : undefined)
